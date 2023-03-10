9da1f6675a4c2f7b2867822966919a5924acc916
aacnchamilton@gmail.com <aacnchamilton@gmail.com>
2023-03-09 18:31:03 -0700

    initial creation of SpringBoot application

---
+++ src/main/java/com/rezourcesched/rez/RezMain.java
@@ -0,0 +1,15 @@
+package com.rezourcesched.rez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezMain {
+
+  public static void main(String[] args) {
+    
+    SpringApplication.run(RezMain.class, args);
+
+  }
+
+}
